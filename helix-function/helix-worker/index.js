6e674ef0995e5a2f618bb7bba81a285471d3c327
sinedied <noda@free.fr>
2022-11-15 15:57:10 +0100

    feat: add proxy function

---
+++ helix-function/helix-worker/index.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import fetch, { Request, Response } from 'node-fetch';
+
+export async function helixWorker(context, req) {
+  console.log({req })
+  const url = new URL(req.url);
+  url.hostname = process.env.HELIX_HOSTNAME;
+
+  const request = new Request(url, req);
+  request.headers.set('x-forwarded-host', request.headers.get('host'));
+  request.headers.set('x-byo-cdn-type', 'azure');
+
+  let res = await fetch(req);
+  res = new Response(res.body, res);
+  res.headers.delete('age');
+  res.headers.delete('x-robots-tag');
+
+  context.res = res;
+}
